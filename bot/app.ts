447625a2d1ede3b20860e5b90fa5c7316e609718
TheSowut <sowutmail@gmail.com>
2021-10-16 22:50:00 +0300

    feat: refactor bot to class & call API functionality

--- bot/app.ts
+++ bot/app.ts
@@ -1,20 +1,56 @@
 import { Client, Message } from "discord.js";
 import { DISCORD_EVENTS } from '../enums/events';
+import { MESSAGE } from '../enums/message';
+import { ENDPOINTS } from '../enums/endpoints';
+import { NWStatusResponse } from '../interfaces/nwStatusWorldsResponse';
 
 // Load the .env file from the root directory.
-require('dotenv').config({path: '../.env'});
+require('dotenv').config({ path: '../.env' });
+class NWQueueBot {
+	public axios = require('axios');
+	public discord = require('discord.js');
+	public client: Client = new this.discord.Client();
 
-const discord = require('discord.js');
-const client: Client = new discord.Client();
+	private config = {
+		headers: {
+			'Authorization': `Bearer ${process.env.NW_BEARER}`
+		},
 
-client.on(DISCORD_EVENTS.READY, () => {
-	console.log(`Logged in as ${client.user!.tag}`);
-})
-
-client.on(DISCORD_EVENTS.MESSAGE, (msg: Message) => {
-	if (msg.content === 'ping') {
-		msg.reply('pong')
 	}
-})
 
-client.login(process.env.TOKEN);
+	constructor() {
+		this.client.login(process.env.TOKEN);
+		this.client.on(DISCORD_EVENTS.READY, () => {
+			console.log(`Logged in as ${this.client.user!.tag}`);
+		})
+
+		this.client.on(DISCORD_EVENTS.MESSAGE, async (msg: Message) => {
+			switch (msg.content) {
+				case 'ping':
+					msg.reply('pong');
+					break;
+				case MESSAGE.QUEUE:
+					const serverInfo: string = JSON.stringify(await this.getWorld('Inferni'));
+					msg.reply(serverInfo);
+					break;
+			};
+		});
+	}
+	/**
+	 * Get information about a specific world.
+	 * @returns NWStatusResponse
+	 */
+	private async getWorld(worldName: string): Promise<NWStatusResponse | undefined> {
+		return await this.axios.get(`${ENDPOINTS.NEW_WORLD_STATUS}?identifier=${worldName}`, this.config)
+			.then((res: NWStatusResponse) => {
+				return res.data.message.worlds.filter((world: any) => {
+					return world.name === worldName
+				});
+			})
+			.catch((err: any) => {
+				console.error(err);
+			})
+	}
+}
+
+new NWQueueBot();
