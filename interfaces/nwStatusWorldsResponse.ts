c2e523fca3c7fb596b46a34f8dc25d25891c79ff
TheSowut <sowutmail@gmail.com>
2021-10-17 14:10:34 +0300

    feat: world interface

--- interfaces/nwStatusWorldsResponse.ts
+++ interfaces/nwStatusWorldsResponse.ts
@@ -1,11 +1,6 @@
+import { WorldInfo } from './world';
 export interface NWStatusResponse {
 	data: {
-		message: {
-			players_current: number,
-			players_maximum: number,
-			queue_current: number,
-			queue_wait_time_minutes: number,
-			status_enum: string
-		}
+		message: WorldInfo
 	}
 }
---
+++ interfaces/world.ts
@@ -0,0 +1,7 @@
+export interface WorldInfo {
+	players_current: number,
+	players_maximum: number,
+	queue_current: number,
+	queue_wait_time_minutes: number,
+	status_enum: string
+}
