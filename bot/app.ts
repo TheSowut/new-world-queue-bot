70014796f26f8e05151da99b050932dd486e035c
TheSowut <sowutmail@gmail.com>
2021-10-17 14:10:47 +0300

    fix: bot replying to himself when server is full

--- bot/app.ts
+++ bot/app.ts
@@ -3,6 +3,7 @@ import { DISCORD_EVENTS } from '../enums/events';
 import { MESSAGE } from '../enums/message';
 import { ENDPOINTS } from '../enums/endpoints';
 import { NWStatusResponse } from '../interfaces/nwStatusWorldsResponse';
+import { WorldInfo } from "../interfaces/world";
 
 // Load the .env file from the root directory.
 require('dotenv').config({ path: '../.env' });
@@ -23,6 +24,8 @@ class NWQueueBot {
 		})
 
 		this.client.on(DISCORD_EVENTS.MESSAGE, async (msg: Message) => {
+			// Early return which will prevent the bot from replying to himself.
+			if (msg.author.id in [this.client.user?.id]) return;
 			// Check whether the user has specified a world name.
 			// If not, check the current queue status for 'inferni'.
 			if (msg.content.includes(MESSAGE.QUEUE)) {
@@ -39,16 +42,14 @@ class NWQueueBot {
 	 * @param worldName
 	 * @returns NWStatusResponse
 	 */
-	private async getWorld(worldName: string): Promise<string | undefined> {
+	private async getWorld(worldName: string): Promise<string> {
 		return await this.axios.get(`${ENDPOINTS.NEW_WORLD_STATUS}/${worldName}`, this.config)
 			.then((res: NWStatusResponse) => {
-				const response = res.data.message;
-				const worldNameCapitalized = this.capitalizeName(worldName);
-				let responseMessage: string = '';
-				responseMessage = response.players_current < response.players_maximum
+				const response: WorldInfo = res.data.message;
+				const worldNameCapitalized: string = this.capitalizeName(worldName);
+				return response.players_current < response.players_maximum
 					? `${worldNameCapitalized} has ${response.players_current} active players out of ${response.players_maximum}.`
-					: `${worldNameCapitalized} is currently FULL. There are ${response.queue_current} players in queue. Current waiting time is ${response.queue_wait_time_minutes} minutes.`
-				return responseMessage;
+					: `${worldNameCapitalized} is currently FULL with ${response.queue_current} players waiting. Current waiting time is ${response.queue_wait_time_minutes} minutes.`
 			})
 			.catch((err: any) => {
 				console.error(err.response.statusText);
